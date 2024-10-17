afd2a36399e8478991fb2f1ad674dd3034d55435
Anton Sheiko <antonstendal19@gmail.com>
2024-10-17 07:45:58 +0200

    Add logInGoogle and logInMicrosoft to Requirements

--- src/main/java/com/sheiko/Requirements.java
+++ src/main/java/com/sheiko/Requirements.java
@@ -2,6 +2,17 @@ package com.sheiko;
 
 public class Requirements {
     public static void main(String[] args) {
+        System.out.println("LogIn must be @mail");
+        System.out.println("Password must be min 8 symbol");
+    }
 
+    public void logInWithGoogle() {
+        System.out.println("LogIn must be @mail");
+        System.out.println("Password must be min 8 symbol");
+    }
+
+    public void logInWithMicrosoft() {
+        System.out.println("LogIn must be @mail");
+        System.out.println("Password must be min 8 symbol");
     }
 }
